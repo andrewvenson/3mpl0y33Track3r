dc0d9ec9273672d7015e75a92f82e39e61f190f4
Andrew Venson <vensoneandrew@gmail.com>
2020-07-11 14:38:10 -0500

    added .env for mysql password

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: `${process.env.MYSQL_PW}`,
     database: "employeetracker"
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log("mysql connected");
+    main();
 });
 
 const main = () => {
