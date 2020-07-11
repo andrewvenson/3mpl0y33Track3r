363807efa10e253a95d54d4081bfd5ea89a2566a
Andrew Venson <vensoneandrew@gmail.com>
2020-07-11 14:43:11 -0500

    connect

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
 
 connection.connect(err => {
     if (err) throw err;
-    console.log("mysql connected");
+    console.log("mysql connection successful");
     main();
 });
 
