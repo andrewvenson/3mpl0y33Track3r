d7b799a89ef40ef4ad7f955c2d3e4151fce022fb
Andrew Venson <vensoneandrew@gmail.com>
2020-07-11 14:10:48 -0500

    empty funcs

--- server.js
+++ server.js
@@ -7,3 +7,31 @@ const connection = mysql.createConnection({
     password: "",
     database: "employeetracker"
 });
+
+const main = () => {
+    inquirer.prompt([]).then(res => {});
+};
+
+const addDept = () => {};
+
+const addRole = () => {};
+
+const addEmp = () => {};
+
+const viewDep = () => {};
+
+const viewRole = () => {};
+
+const viewEmp = () => {};
+
+const updateMgr = () => {};
+
+const viewEmpByMgr = () => {};
+
+const delDep = () => {};
+
+const delRole = () => {};
+
+const delEmp = () => {};
+
+const viewBud = () => {};
