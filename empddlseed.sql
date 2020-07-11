bea87243b133e023808c06716f581898c3912003
Andrew Venson <vensoneandrew@gmail.com>
2020-07-11 14:18:51 -0500

    redesigned schema for salary

--- empddlseed.sql
+++ empddlseed.sql
@@ -12,7 +12,6 @@ CREATE TABLE department(
 CREATE TABLE role(
     role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     title VARCHAR(50) NOT NULL,
-    salary DECIMAL(10,2) NOT NULL,
     dept_id INT NOT NULL, 
     FOREIGN KEY (dept_id) REFERENCES department(dept_id)
 );
@@ -22,6 +21,7 @@ CREATE TABLE employees(
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     role_id INT NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
     manager_id INT NULL, 
     FOREIGN KEY (role_id) REFERENCES role(role_id)
 );
